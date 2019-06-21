7a8673c5ea23cf5f6e1a46f37c91dbb13903a718
Jose Marco <mbros9@gmail.com>
2019-06-21 20:38:11 -0300

    a

--- Guild Godot/Title Screen.gd
+++ Guild Godot/Title Screen.gd
@@ -8,7 +8,6 @@ func _on_Button_pressed():
 	get_tree().change_scene("res://Map.tscn")
 
 func _on_Button2_pressed():
-	get_tree().set_pause(true)
 	get_tree().change_scene("res://Credits.tscn")
 	
 func _on_Button3_pressed():
