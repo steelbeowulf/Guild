7a995dcc6d8e3752894d814b122d555ebab49716
Jose Marco <mbros9@gmail.com>
2020-01-28 18:59:12 -0300

    Added a scroll on the item menu on battle( still have to add more stuff so it doesn't crash with more itens)

--- Guild Godot/Battle/Menus/BigActionButton.gd
+++ Guild Godot/Battle/Menus/BigActionButton.gd
@@ -4,19 +4,19 @@ var item_picked = null
 signal action_picked
 
 func _ready():
-	for c in $Targets/HBoxContainer/Itens.get_children():
+	for c in $Targets/ItemContainer/HBoxContainer/Itens.get_children():
 		c.connect("target_picked", self, "_on_Item_Picked")
-	for c in $Targets/HBoxContainer/Players.get_children():
+	for c in $Targets/PlayerContainer/HBoxContainer/Players.get_children():
 		c.connect("target_picked", self, "_on_Target_Picked")
 		c.disabled = true
-	for c in $Targets/HBoxContainer/Enemies.get_children():
+	for c in $Targets/EnemiesContainer/HBoxContainer/Enemies.get_children():
 		c.connect("target_picked", self, "_on_Target_Picked")
 		c.disabled = true
 
 func connect_targets(list_players, list_enemies, manager):
 	var id = 0
 	var img
-	for e in $Targets/HBoxContainer/Enemies.get_children():
+	for e in $Targets/EnemiesContainer/HBoxContainer/Enemies.get_children():
 		if id < len(list_enemies):
 			img = list_enemies[id]
 			e.connect("focus_entered", img, "turn")
@@ -25,7 +25,7 @@ func connect_targets(list_players, list_enemies, manager):
 			e.connect("focus_exited", manager, "manage_hate", [1, id])      
 			id += 1
 	id = 0
-	for e in $Targets/HBoxContainer/Players.get_children():
+	for e in $Targets/PlayerContainer/HBoxContainer/Players.get_children():
 		if id < len(list_players):
 			img = list_players[id]
 			e.connect("focus_entered", img, "turn")
@@ -41,17 +41,23 @@ func _on_Target_Picked(target):
 	if item_picked != null:
 		target = [item_picked, target]
 		emit_signal("action_picked", self.text, target)
-		$Targets/HBoxContainer.hide()
+		$Targets/ItemContainer/HBoxContainer.hide()
+		$Targets/PlayerContainer/HBoxContainer.hide()
+		$Targets/EnemiesContainer/HBoxContainer.hide()
 		print("emitindo sinal action_picked do "+str(self.text))
 
 func _on_Action_pressed():
 	$Targets.show()
-	$Targets/HBoxContainer.show()
-	get_node("Targets/HBoxContainer/Itens/0").grab_focus()
-	for e in $Targets/HBoxContainer/Enemies.get_children():
+	$Targets/ItemContainer/HBoxContainer.show()
+	$Targets/PlayerContainer/HBoxContainer.show()
+	$Targets/EnemiesContainer/HBoxContainer.show()
+	get_node("Targets/ItemContainer/HBoxContainer/Itens/0").grab_focus()
+	for e in $Targets/EnemiesContainer/HBoxContainer/Enemies.get_children():
 		e.disabled = true
-	for p in $Targets/HBoxContainer/Players.get_children():
+	for p in $Targets/PlayerContainer/HBoxContainer/Players.get_children():
 		p.disabled = true
+
+
 
 func _on_Item_Picked(item):
 	item_picked = item
@@ -60,19 +66,27 @@ func _on_Item_Picked(item):
 	if item.get_type() == "RESSURECTION":
 		ress = true
 	if item.get_target() == "ALL":
-		for e in $Targets/HBoxContainer/Enemies.get_children():
+		for e in $Targets/EnemiesContainer/HBoxContainer/Enemies.get_children():
 			e.hide()
-		for p in $Targets/HBoxContainer/Players.get_children():
+		for p in $Targets/PlayerContainer/HBoxContainer/Players.get_children():
 			p.hide()
-		$Targets/HBoxContainer/Enemies/"E0".set_text("All Enemies")
-		$Targets/HBoxContainer/Enemies/"E0".disabled = false
-		$Targets/HBoxContainer/Enemies/"E0".show()
-		$Targets/HBoxContainer/Players/"P0".set_text("All Players")
-		$Targets/HBoxContainer/Players/"P0".disabled = false
-		$Targets/HBoxContainer/Players/"P0".show()
+		$Targets/EnemiesContainer/HBoxContainer/Enemies/"E0".set_text("All Enemies")
+		$Targets/EnemiesContainer/HBoxContainer/Enemies/"E0".disabled = false
+		$Targets/EnemiesContainer/HBoxContainer/Enemies/"E0".show()
+		$Targets/PlayerContainer/HBoxContainer/Players/"P0".set_text("All Players")
+		$Targets/PlayerContainer/HBoxContainer/Players/"P0".disabled = false
+		$Targets/PlayerContainer/HBoxContainer/Players/"P0".show()
 	else:
-		for e in $Targets/HBoxContainer/Enemies.get_children():
+		for e in $Targets/EnemiesContainer/HBoxContainer/Enemies.get_children():
 			e.disabled = false
-		for p in $Targets/HBoxContainer/Players.get_children():
+		for p in $Targets/PlayerContainer/HBoxContainer/Players.get_children():
 			p.disabled = false
-	$Targets/HBoxContainer/Players/"P0".grab_focus()
+	$Targets/PlayerContainer/HBoxContainer/Players/"P0".grab_focus()
+
+func _on_4_focus_exited():
+	if Input.is_action_pressed("ui_down"):
+		$Targets/ItemContainer.scroll_vertical += 20
+	if Input.is_action_pressed("ui_up"):
+		$Targets/ItemContainer.scroll_vertical -= 20
+	if Input.is_action_pressed("ui_cancel"):
+		$Targets/ItemContainer.scroll_vertical = 0
