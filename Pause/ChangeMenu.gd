2ab5a49e5efa831dae4611e9c706c51b1cbe7b4a
Jose Marco <mbros9@gmail.com>
2021-05-01 17:37:51 -0300

    Change Characters Close to finish

--- Pause/ChangeMenu.gd
+++ Pause/ChangeMenu.gd
@@ -14,21 +14,18 @@ func _ready():
 		c.connect("target_selected", self, "_on_Change_hover", [i])
 	for btn in $Panel/HBoxContainer/Options.get_children():
 		btn.connect("focus_entered", self, "_on_Focus_Entered")
-	get_node("Panel/HBoxContainer/Options/Player 1").grab_focus()
+	get_node("Panel/HBoxContainer/Options/Player1").grab_focus()
 	for e in $Panel/HBoxContainer/Options.get_children():
 		e.set_focus_mode(0)
 
-#func join_ranger():
-#	if len(GLOBAL.PLAYERS) < 4:
-#		var loader = GLOBAL.get_root().get_parent().get_node("/root/LOADER")
-#		GLOBAL.PLAYERS.append(loader.parse_players(RANGER_PATH)[0])
+
 
 func just_entered(id):
 	print("[EQUIP] just entered "+str(id))
 	player = GLOBAL.PLAYERS[id]
 	location = "SUBMENU"
 
-func show_reserve(id):
+func show_reserve():
 	reset_info()
 	# Ponto inicial da lista
 	var loader = GLOBAL.get_root().get_parent().get_node("/root/LOADER")
@@ -39,23 +36,16 @@ func show_reserve(id):
 		node.show()
 		node.disabled = false
 
-# Get first equippable item from equip list
-# Avoids giving focus to wrong job itens
-func get_first_equippable():
-	for i in range(len(equips)):
-		if equips[i] != null and equips[i].job == player.job:
-			return get_node("Panel/HBoxContainer/Equips/EquipSlot" + str(i))
 
-# Equip item with id
-func _on_Equip_selected(id):
+#Change the players in party to the reserve
+func _on_Change_selected(id):
 	AUDIO.play_se("ENTER_MENU")
-	use_equip(equips[id])
+	change_players(id)
 
 
-func _on_Equip_hover(id):
+func _on_Change_hover(id):
 	AUDIO.play_se("MOVE_MENU")
-	set_description(equips[id])
-	$Panel/HBoxContainer/Options/Info/Comparison.init(current_equip, equips[id])
+	#set_description(equips[id])
 
 # Reset info panel
 func reset_info():
@@ -82,34 +72,38 @@ func _process(delta):
 
 func give_focus():
 	print("Giving focus...")
-	$Panel/HBoxContainer/Options/Weapon.set_focus_mode(2)
-	$Panel/HBoxContainer/Options/Head.set_focus_mode(2)
-	$Panel/HBoxContainer/Options/Body.set_focus_mode(2)
-	$Panel/HBoxContainer/Options/Accessory1.set_focus_mode(2)
-	$Panel/HBoxContainer/Options/Accessory2.set_focus_mode(2)
+	$Panel/HBoxContainer/Options/Player1.set_focus_mode(2)
+	$Panel/HBoxContainer/Options/Player2.set_focus_mode(2)
+	$Panel/HBoxContainer/Options/Player3.set_focus_mode(2)
+	$Panel/HBoxContainer/Options/Player4.set_focus_mode(2)
 	$Panel/HBoxContainer/Options/Back.set_focus_mode(2)
-	for e in $Panel/HBoxContainer/Equips.get_children():
+	for e in $Panel/HBoxContainer/Reserve.get_children():
 		e.set_focus_mode(0)
 		e.hide()
-	get_node("Panel/HBoxContainer/Options/Weapon").grab_focus()
+	get_node("Panel/HBoxContainer/Options/Player1").grab_focus()
 	reset_info()
 
 
 func enter():
 	give_focus()
-	get_node("Panel/HBoxContainer/Options/Weapon").grab_focus()
+	get_node("Panel/HBoxContainer/Options/Player1").grab_focus()
 
 
-# Can't unequip for now
-func use_equip(equip):
+#func join_ranger():
+#	if len(GLOBAL.PLAYERS) < 4:
+#		var loader = GLOBAL.get_root().get_parent().get_node("/root/LOADER")
+#		GLOBAL.PLAYERS.append(loader.parse_players(RANGER_PATH)[0])
+# Trade player from the reserve to the active party
+func change_players(id):
 	AUDIO.play_se("ENTER_MENU")
-	if location == "ACCESSORY1":
-		player.equip(equip, 3)
-	elif location == "ACCESSORY2":
-		player.equip(equip, 4)
-	else:
-		player.equip(equip)
-	show_equips(GLOBAL.EQUIP_INVENTORY, location)
+	#player.equip(equip)
+	#removing the player and adding it to the reserve
+	
+	
+	#adding the player
+	var loader = GLOBAL.get_root().get_parent().get_node("/root/LOADER")
+	GLOBAL.PLAYERS.append(loader.parse_players(RESERVE_PATH)[id])
+	show_reserve()
 
 func _on_Back_pressed():
 	AUDIO.play_se("EXIT_MENU")
@@ -121,66 +115,12 @@ func _on_Back_pressed():
 func _on_Focus_Entered():
 	AUDIO.play_se("MOVE_MENU")
 
-func _on_Weapon_pressed():
-	location = "WEAPON"
-	show_equips(GLOBAL.EQUIP_INVENTORY, location)
+func _on_Player1_pressed():
+	location = "CHANGE"
+	show_reserve()
 	AUDIO.play_se("ENTER_MENU")
 	for e in $Panel/HBoxContainer/Options.get_children():
 		e.set_focus_mode(0)
-	for e in $Panel/HBoxContainer/Equips.get_children():
+	for e in $Panel/HBoxContainer/Reserve.get_children():
 		e.set_focus_mode(2)
-	var first = get_first_equippable()
-	if first:
-		first.grab_focus()
-
-
-func _on_Head_pressed():
-	location = "HEAD"
-	show_equips(GLOBAL.EQUIP_INVENTORY, location)
-	AUDIO.play_se("ENTER_MENU")
-	for e in $Panel/HBoxContainer/Options.get_children():
-		e.set_focus_mode(0)
-	for e in $Panel/HBoxContainer/Equips.get_children():
-		e.set_focus_mode(2)
-	var first = get_first_equippable()
-	if first:
-		first.grab_focus()
-
-
-func _on_Body_pressed():
-	location = "BODY"
-	show_equips(GLOBAL.EQUIP_INVENTORY, location)
-	AUDIO.play_se("ENTER_MENU")
-	for e in $Panel/HBoxContainer/Options.get_children():
-		e.set_focus_mode(0)
-	for e in $Panel/HBoxContainer/Equips.get_children():
-		e.set_focus_mode(2)
-	var first = get_first_equippable()
-	if first:
-		first.grab_focus()
-
-
-func _on_Acessory1_pressed():
-	location = "ACCESSORY1"
-	show_equips(GLOBAL.EQUIP_INVENTORY, location)
-	AUDIO.play_se("ENTER_MENU")
-	for e in $Panel/HBoxContainer/Options.get_children():
-		e.set_focus_mode(0)
-	for e in $Panel/HBoxContainer/Equips.get_children():
-		e.set_focus_mode(2)
-	var first = get_first_equippable()
-	if first:
-		first.grab_focus()
-
-
-func _on_Acessory2_pressed():
-	location = "ACCESSORY2"
-	show_equips(GLOBAL.EQUIP_INVENTORY, location)
-	AUDIO.play_se("ENTER_MENU")
-	for e in $Panel/HBoxContainer/Options.get_children():
-		e.set_focus_mode(0)
-	for e in $Panel/HBoxContainer/Equips.get_children():
-		e.set_focus_mode(2)
-	var first = get_first_equippable()
-	if first:
-		first.grab_focus()
+	get_node("Panel/HBoxContainer/Reserve/ReserveSlot1").grab_focus()
